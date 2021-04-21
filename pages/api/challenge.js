b0328cea106a2340837702c6d105847058e7992d
Ernestas Burokas <erniukas86@gmail.com>
2021-04-22 00:55:51 +0300

    Add error to response

--- pages/api/challenge.js
+++ pages/api/challenge.js
@@ -16,7 +16,7 @@ export default async (req, res) => {
       res.status(200).json(result);
     } catch (error) {
       console.log(error);
-      res.status(200).json("");
+      res.status(500).json(error);
     }
 }
 
