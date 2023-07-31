bb56b45680c97b89fdb5a7ad9f3aa214123eb6a3
Ernestas Burokas <ernestas.burokas@presentconnection.eu>
2023-07-31 23:10:40 +0300

    reset

--- pages/api/config.js
+++ pages/api/config.js
@@ -4,7 +4,7 @@ export const config = {
   refreshToken: '0c3867795fc7196384bd4f85c472860fff40b985',
   stravaApiUrl: 'https://www.strava.com/api/v3',
   challengeName: 'PC CYCLING CHALLENGE 2023',
-  challengeStartId: '40249.7919913423103.1',
+  challengeStartId: '81048.51482018236438.8',
   challengeTeamRecord: 9556.85,
   challengeDescription: 'Rewards: >200Km: individual medals. Top 3: Special prizes and infinite glory.',
   kmToGermany: 1279,
