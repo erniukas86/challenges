381c7e6c747fe6cae1d03b5420c592c4846e32bf
Ernestas Burokas <ernestas.burokas@presentconnection.eu>
2024-06-05 17:11:46 +0300

    Updates

--- pages/api/config.js
+++ pages/api/config.js
@@ -4,7 +4,7 @@ export const config = {
   refreshToken: '0c3867795fc7196384bd4f85c472860fff40b985',
   stravaApiUrl: 'https://www.strava.com/api/v3',
   challengeName: 'PC RUNNING CHALLENGE 2024',
-  challengeStartId: '15264.15740587128',
+  challengeStartId: '8009.1269826983.6',
   challengeTeamRecord: 5764.21,
   challengeDescription: 'Rewards: >150Km: individual medals. Top 3: Special prizes and infinite glory.',
   kmToGermany: 1279,
