4e1693da8b849d210f6ea6a61610b48cd63a5ccd
Ernestas Burokas <ernestas.burokas@presentconnection.eu>
2024-08-20 17:42:33 +0300

    Fixed URL for getting club activities

--- pages/api/config.js
+++ pages/api/config.js
@@ -4,7 +4,7 @@ export const config = {
   refreshToken: '0c3867795fc7196384bd4f85c472860fff40b985',
   stravaApiUrl: 'https://www.strava.com/api/v3',
   challengeName: 'PC CYCLING CHALLENGE 2024',
-  challengeStartId: '7898.41554156069.7',
+  challengeStartId: '10003.22152350766.5',
   challengeTeamRecord: 10250,
   challengeDescription: 'Rewards: >300Km: individual medals. Top 3: Special prizes and infinite glory.',
   kmToGermany: 1279,
@@ -14,5 +14,5 @@ export const config = {
 
 export const stravaEndpoints = {
   GET_ACCESS_TOKEN: `${config.stravaApiUrl}/oauth/token`,
-  GET_CLUB_WORKOUTS: `${config.stravaApiUrl}/clubs/775938/activities?per_page=200?page=0`,
+  GET_CLUB_WORKOUTS: `${config.stravaApiUrl}/clubs/775938/activities?per_page=200`,
 };
